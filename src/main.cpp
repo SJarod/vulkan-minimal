26893c14509dee2599ad536a159f41cdec05af0c
Jarod SENGKEO <j.sengkeo@student.isartdigital.com>
2022-06-15 11:25:26 +0200

    validation layers

--- src/main.cpp
+++ src/main.cpp
@@ -4,85 +4,112 @@
 #include <vector>
 #include <iostream>
 
+const std::vector<const char*> validationLayers = {
+	"VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
 VkInstance instance;
 
 int vulkanInit()
 {
-    if (!glfwVulkanSupported())
-    {
-        std::cerr << "GLFW failed to find the Vulkan loader.\nExiting ...\n";
-        fflush(stdout);
-        return -1;
-    }
+	if (!glfwVulkanSupported())
+	{
+		std::cerr << "GLFW failed to find the Vulkan loader.\nExiting ...\n";
+		fflush(stdout);
+		return -1;
+	}
 
-    if (!gladLoaderLoadVulkan(nullptr, nullptr, nullptr))
-    {
-        std::cerr << "Unable to load Vulkan symbols!\ngladLoad Failure\n";
-        return -2;
-    }
+	if (!gladLoaderLoadVulkan(nullptr, nullptr, nullptr))
+	{
+		std::cerr << "Unable to load Vulkan symbols!\ngladLoad Failure\n";
+		return -2;
+	}
 }
 
 int vulkanCreate()
 {
-    VkApplicationInfo appInfo = {
-        .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
-        .pApplicationName = "Hello triangle",
-        .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
-        .engineVersion = VK_MAKE_VERSION(1, 0, 0),
-        .apiVersion = VK_API_VERSION_1_0
-    };
+	VkApplicationInfo appInfo = {
+		.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+		.pApplicationName = "Hello triangle",
+		.applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+		.engineVersion = VK_MAKE_VERSION(1, 0, 0),
+		.apiVersion = VK_API_VERSION_1_0
+	};
 
-    uint32_t glfwExtensionCount = 0;
-    const char** glfwExtensions;
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions;
 
-    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
-    VkInstanceCreateInfo createInfo = {
-        .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
-        .pApplicationInfo = &appInfo,
-        .enabledLayerCount = 0,
-        .enabledExtensionCount = glfwExtensionCount,
-        .ppEnabledExtensionNames = glfwExtensions
-    };
+	VkInstanceCreateInfo createInfo = {
+		.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+		.pApplicationInfo = &appInfo,
+		.enabledLayerCount = 0,
+		.enabledExtensionCount = glfwExtensionCount,
+		.ppEnabledExtensionNames = glfwExtensions,
+	};
+	if (enableValidationLayers)
+	{
+		createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+		createInfo.ppEnabledLayerNames = validationLayers.data();
+	}
 
-    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
-    {
-        std::cerr << "Failed to create vulkan instance\n";
-        return -1;
-    }
+	if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+	{
+		std::cerr << "Failed to create vulkan instance\n";
+		return -1;
+	}
 }
 
 void vulkanExtensions()
 {
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-    std::vector<VkExtensionProperties> extensions(extensionCount);
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
-    std::cout << "available extensions : " << extensionCount << '\n';
-    for (const auto& extension : extensions)
-        std::cout << '\t' << extension.extensionName << '\n';
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+	std::vector<VkExtensionProperties> extensions(extensionCount);
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+	std::cout << "available extensions : " << extensionCount << '\n';
+	for (const auto& extension : extensions)
+		std::cout << '\t' << extension.extensionName << '\n';
+}
+
+void vulkanLayers()
+{
+	uint32_t layerCount = 0;
+	vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+	std::vector<VkLayerProperties> layers(layerCount);
+	vkEnumerateInstanceLayerProperties(&layerCount, layers.data());
+	std::cout << "available layers : " << layerCount << '\n';
+	for (const auto& layer : layers)
+		std::cout << '\t' << layer.layerName << '\n';
 }
 
 int main()
 {
-    glfwInit();
-    if (vulkanInit() < 0) return -1;
-    if (vulkanCreate() < 0) return -2;
-    vulkanExtensions();
+	glfwInit();
+	if (vulkanInit() < 0) return -1;
+	if (vulkanCreate() < 0) return -2;
+	vulkanExtensions();
+	vulkanLayers();
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
 
-    while (!glfwWindowShouldClose(window))
-    {
-        glfwPollEvents();
-    }
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
 
-    vkDestroyInstance(instance, nullptr);
+	vkDestroyInstance(instance, nullptr);
 
-    glfwDestroyWindow(window);
+	glfwDestroyWindow(window);
 
-    glfwTerminate();
+	glfwTerminate();
 
-    return 0;
+	return 0;
 }
